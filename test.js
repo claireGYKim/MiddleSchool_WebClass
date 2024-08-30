6eaa396230188f6e08ee59baf2506305db41f895
Claire Gyeong_Yeon Kim <146415768+claireGYKim@users.noreply.github.com>
2024-08-30 23:15:45 +0900

    ExampleCode for MiddleSchool WebClass

---
+++ test.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // content 요소를 가져와서 애니메이션 효과를 적용합니다.
+    var content = document.querySelector(".content");
+    content.style.opacity = 5;
+});
